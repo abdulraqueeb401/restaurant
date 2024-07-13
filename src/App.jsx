195c807b52785eaadd6910c140dda5ac8154537e
abdulraqueeb401 <abdulraqueeb2000@gmail.com>
2024-07-13 15:35:15 +0530

    simple sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import React from "react";
+// import {useState} from "react";
 import Card from "./components/Card";
 import sampledata from "./utilities/sampledata.json";
+import Sidebar from "./components/Sidebar";
 
 const App = () => {
+  // const [state, setState] = useState({})
   const cardlist = new Array(10)
     .fill(0)
     .map((item, index) => <Card key={index} />);
-  console.log(cardlist);
+  const categories = sampledata.categories.map((item) => item.name);
   return (
-    <div className="grid grid-cols-4 ">
-      <div className="flex flex-col gap-y-2 "></div>
+    <div className="grid grid-cols-4 gap-x-4">
+      <Sidebar categories={categories} />
       <div className="flex flex-col gap-y-5 col-span-2">{cardlist}</div>
     </div>
   );
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,26 @@
+import PropTypes from "prop-types";
+
+const Sidebar = ({ categories }) => {
+  const handleClick = (key) => {
+    console.log(key);
+  };
+  return (
+    <div className="flex flex-col gap-y-2 pt-4">
+      {categories.map((category, index) => (
+        <h3
+          key={index}
+          onClick={() => handleClick(category)}
+          className="pl-5 active:bg-gradient-to-r hover:bg-gradient-to-r from-white to-red-500 cursor-pointer"
+        >
+          {category}
+        </h3>
+      ))}
+    </div>
+  );
+};
+
+Sidebar.propTypes = {
+  categories: PropTypes.array.isRequired,
+};
+
+export default Sidebar;
